79e1b746ab740163c1083a04646d85fa7c515ad1
ahmodadeola <ahmodadeola@gmail.com>
2021-08-17 13:37:13 +0100

    modified Parser to use dynamically set root path

--- Core/Parser.cs
+++ Core/Parser.cs
@@ -1,28 +1,43 @@
-using System.Data;
 using iTextSharp.text;   
 using iTextSharp.text.pdf;  
 using iTextSharp.text.pdf.parser;  
 using System;
 using System.Text;
+using System.IO;
+using System.Web;
 
 namespace Search_Engine_Project.Core
 {
     public class Parser
     {
+        private static string _rootPath = getRootPath();
         public static string GetTextFromPDF(){  
             try{
                 StringBuilder text = new StringBuilder();  
-                using (PdfReader reader = new PdfReader(@"C:\Users\USER\Downloads\CourseRegistration Ahmad Salaudeen.pdf")){  
+                //form file path
+                string filePath = System.IO.Path.Combine(_rootPath, "docs", "Stoichiometry AP Exam Questions.pdf");
+                Console.WriteLine(_rootPath);
+                using (PdfReader reader = new PdfReader(filePath)){  
                     for (int i = 1; i <= reader.NumberOfPages; i++){  
                         text.Append(PdfTextExtractor.GetTextFromPage(reader, i));  
                     }  
                 }  
                 Console.WriteLine(text.ToString());
                 return text.ToString();
-            }catch(Exception err){
-                Console.WriteLine(err);
+            }catch(Exception ex){
+                Console.WriteLine("An error occured");
+                return "";
+                
             }
             return "";
         }  
+
+        private static string getRootPath(){
+            string _rootPath = Directory.GetParent(System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory)).FullName;
+            _rootPath = _rootPath.Substring(0, _rootPath.Length-9);
+            return _rootPath;
+        }
     }
+
+    
 }
