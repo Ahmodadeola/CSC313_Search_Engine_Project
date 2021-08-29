a50e9f1bf790c68abadb309c1b85f695130d02af
dazco <dazco777@gmail.com>
2021-08-29 06:38:01 +0100

    added search controller endpoint

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Search_Engine_Project.Core;
+using Search_Engine_Project.Models;
+using Search_Engine_Project.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Search_Engine_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly WordService _WordService;
+
+        public SearchController(WordService WordService)
+        {
+            _WordService = WordService;
+        }
+        // GET: api/<SearchController>
+        [HttpGet]
+        public ActionResult<List<string>> Get(string query)
+        {
+            List<String> splits = Semanter.Splitwords(query).ToList();
+            HashSet<string> uniqueSplits = splits.ToHashSet();
+            List<Word> words = _WordService.FindByKeywords(uniqueSplits);
+            return (new string[] { "value1", "value2" }).ToList();
+        }
+    }
+}
